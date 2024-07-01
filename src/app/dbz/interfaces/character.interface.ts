b96c959b1ca573054a32635821ffa324bfc2ce4e
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-07-01 14:35:12 -0600

    Paquetes externos - UUID (tarea - pending)

--- src/app/dbz/interfaces/character.interface.ts
+++ src/app/dbz/interfaces/character.interface.ts
@@ -1,6 +1,8 @@
+import { v4 as uuid } from 'uuid';
 
 
 export interface Character {
+  id?: string;
   name: string;
   power: number;
 }
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,8 @@
+import { Character } from './../interfaces/character.interface';
 import { Injectable } from '@angular/core';
-import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid'
+
+console.log(uuid());
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +12,32 @@ export class DbzService {
 
 
   public characters: Character[] = [{
+    id: uuid(),
     name: "krilin",
     power: 1000,
   }, {
+    id: uuid(),
     name: "Goku",
     power: 9500,
   }, {
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }]
 
   onNewCharacter( character: Character): void {
-    this.characters.push(character);
+
+    const newCharacter: Character = { id: uuid(), ...character };
+
+    this.characters.push(newCharacter);
 
   }
 
-  onDeleteCharacter( index:number ) {
-    this.characters.splice(index, 1);
+  // onDeleteCharacter( index:number ) {
+  //   this.characters.splice(index, 1);
+  deleteCharacterById( id: string ){
+    this.characters = this.characters.filter( character => character.id !== id);
+
   }
 
 }
