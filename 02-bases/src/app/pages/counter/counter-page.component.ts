9b306754e2f8ce487848eb2141f38c20c2bbb723
CesarLinux <eduardomelchor34@gmail.com>
2026-03-07 10:37:59 -0600

    seniales

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
 
@@ -14,14 +14,17 @@ import { Component } from "@angular/core";
 export class CounterPageComponent {
   counter = 10;
 
+  counterSignal = signal(10);
+
   increaseBy(value: number) {
     this.counter += value;
-  }
-
-  decreaseBy(value: number) {
-    this.counter -= value;
+    this.counterSignal.update((current) => current + value);
   }
   resetCounter(value: number) {
     this.counter = 10;
+
+    this.counterSignal.set(0)
   }
+
+
 }
