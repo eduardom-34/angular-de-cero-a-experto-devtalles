fdad361ec03ca4dfaffc8bdfe57d9c81689f9bae
CesarLinux <eduardomelchor34@gmail.com>
2026-03-14 09:23:34 -0600

    Función input - Comunicación entre componentes

---
+++ 02-bases/src/app/components/dragonball/character-list/character-list.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Character } from '../../../interfaces/character.interface';
+
+@Component({
+  selector: 'dragonball-character-list',
+  imports: [],
+  templateUrl: './character-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CharacterListComponent {
+  characters = input.required<Character[]>();
+}
--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -10,15 +10,15 @@ interface Character {
 @Component({
   templateUrl: './dragonball-page.component.html',
 })
-export class DragonballComponent {
-  name = signal('Gohan');
-  power = signal(100);
+export class DragonballPageComponent {
+  name = signal('');
+  power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 4, name: 'Yamcha', power: 500 },
-    { id: 3, name: 'Piccolo', power: 3000 },
+    // { id: 2, name: 'Vegeta', power: 8000 },
+    // { id: 4, name: 'Yamcha', power: 500 },
+    // { id: 3, name: 'Piccolo', power: 3000 },
   ]);
 
 
