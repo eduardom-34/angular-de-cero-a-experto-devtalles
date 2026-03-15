15e1fe4888d9d9adf1d85d819f30708a5f13717e
CesarLinux <eduardomelchor34@gmail.com>
2026-03-15 12:14:55 -0600

    Servicios en Angular

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,14 +1,8 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { CharacterAddComponent } from "../../components/dragonball/character-app/character-add.component";
+import { DragonballService } from "../../services/dragonball.service";
 
-
-
-interface Character {
-  id: number;
-  name: string;
-  power: number
-}
 
 @Component({
   templateUrl: './dragonball-super-page.component.html',
@@ -16,20 +10,5 @@ interface Character {
   imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DragonballSuperPageComponent {
-  name = signal('');
-  power = signal(0);
-
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
-
-  addCharacter(character: Character) {
-    this.characters.update((list) => [...list, character]);
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
+  public dragonballService = inject(DragonballService);
 }
