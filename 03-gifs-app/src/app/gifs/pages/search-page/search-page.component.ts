5e6e1a367468fa0adedcef43614bdcd483340597
CesarLinux <eduardomelchor34@gmail.com>
2026-03-28 20:59:49 -0600

    Diseño de pantalla - Buscador de Gifs

--- 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+
+  gifService = inject(GifsService);
+
+  onSearch( query:string ){
+    this.gifService.searchGifs(query);
+  }
+
+ }
--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -35,4 +35,18 @@ export class GifsService {
 
   }
 
+  searchGifs(query: string) {
+
+    this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q: query,
+      },
+    }).subscribe( ( resp ) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray( resp.data );
+      console.log({ searcch: gifs });
+    })
+
+  }
 }
