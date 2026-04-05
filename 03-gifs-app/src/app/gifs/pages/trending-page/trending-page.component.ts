6200fa2dd7279dc1e6f4e09fc5d0bc2618bc4f9e
CesarLinux <eduardomelchor34@gmail.com>
2026-04-04 19:37:04 -0600

    Determinar fin de scroll

--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -12,13 +12,21 @@ import { GifsService } from '../../services/gifs.service';
 export default class TrendingPageComponent {
 
   gifService = inject(GifsService);
-  scrollDivRef = viewChild<ElementRef>('groupDiv')
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
 
 
   onScroll( event: Event){
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if( !scrollDiv ) return;
 
-    console.log(scrollDiv);
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    // console.log({ scrollTotal: scrollTop + clientHeight, scrollHeight });
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    console.log({ isAtBottom });
   }
 
 }
