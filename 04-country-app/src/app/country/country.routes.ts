07e30bf2af89b30d2d1cec000d1402c17f140c1e
CesarLinux <eduardomelchor34@gmail.com>
2026-04-10 09:23:34 -0600

    Rutas y estructura de carpetas

---
+++ 04-country-app/src/app/country/country.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    component: ByCapitalPageComponent,
+  },
+
+  // {
+  //   path: 'country',
+  //   // ???
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: '',
+  // },
+];
+
+export default countryRoutes;
