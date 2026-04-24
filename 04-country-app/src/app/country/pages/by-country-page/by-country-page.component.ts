4ea50a1d89c0d2c0e1d88e5be1fc4cf56138f830
CesarLinux <eduardomelchor34@gmail.com>
2026-04-23 21:10:43 -0600

    Tarea - Buscar países

--- 04-country-app/src/app/country/pages/by-country-page/by-country-page.component.ts
+++ 04-country-app/src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-country-page',
@@ -10,4 +12,18 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCountryPageComponent {
   countries = signal<Country[]>([]);
+
+  countryService = inject(CountryService);
+  query = signal('');
+
+  countryResources = resource({
+    request: () => ({ query: this.query()}),
+    loader: async({ request }) => {
+      if( !request.query) return
+
+      return await firstValueFrom(
+        this.countryService.searchByCountry(request.query)
+      )
+    }
+  })
 }
--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -8,25 +8,38 @@ import { CountryMapper } from '../mappers/country.mappers';
 const API_URL = 'https://restcountries.com/v3.1';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountryService {
   private http = inject(HttpClient);
 
-  searchByCapital( query: string ): Observable<Country[]> {
+  searchByCapital(query: string): Observable<Country[]> {
     query = query.toLocaleLowerCase();
 
-    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-    .pipe(
-      map( (resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
-      catchError( error => {
-        console.log("Error fetching ", error);
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.log('Error fetching ', error);
 
         return throwError(() =>
           Error(`No se pudo obtener los paises con ese query: ${query}`),
         );
-      })
-    )
+      }),
+    );
   }
 
+  searchByCountry(query: string): Observable<Country[]> {
+    query = query.toLocaleLowerCase();
+
+    return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.log('Error fetching ', error);
+
+        return throwError(() =>
+          Error(`No se pudo obtener los paises con ese query: ${query}`),
+        );
+      }),
+    );
+  }
 }
