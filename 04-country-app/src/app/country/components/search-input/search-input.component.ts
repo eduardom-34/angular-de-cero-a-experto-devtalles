43bff1402a10308a7ac353f24beb1d0491d161e1
CesarLinux <eduardomelchor34@gmail.com>
2026-04-29 21:26:54 -0600

    Debounce - Búsquedas automáticas

--- 04-country-app/src/app/country/components/search-input/search-input.component.ts
+++ 04-country-app/src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'country-search-input',
@@ -6,8 +7,21 @@ import { Component, input, output } from '@angular/core';
   templateUrl: './search-input.component.html',
 })
 export class SearchInputComponent {
-
   placeholder = input('Buscar');
   value = output<string>();
+  debounceTime = input(500);
 
+  inputValue = signal<string>('');
+
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => clearTimeout(timeout));
+
+  })
 }
--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mappers';
 
@@ -14,6 +14,7 @@ export class CountryService {
   private http = inject(HttpClient);
 
   searchByCapital(query: string): Observable<Country[]> {
+
     query = query.toLocaleLowerCase();
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
