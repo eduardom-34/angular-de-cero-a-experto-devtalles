41195b11c5a8c50fc15c9e69b9cf6fae7cbbb0b8
CesarLinux <eduardomelchor34@gmail.com>
2026-04-30 12:18:27 -0600

    Caché de resultados

--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { map, Observable, catchError, throwError, delay, of } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of, tap } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mappers';
 
@@ -11,14 +11,24 @@ const API_URL = 'https://restcountries.com/v3.1';
   providedIn: 'root',
 })
 export class CountryService {
+
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
+
 
   searchByCapital(query: string): Observable<Country[]> {
 
     query = query.toLocaleLowerCase();
 
+    if ( this.queryCacheCapital.has(query)){
+      return of( this.queryCacheCapital.get(query) ??[]);
+    }
+
+    console.log(`Llegando al servidor por ${query}`);
+
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap( countries => this.queryCacheCapital.set(query, countries) ),
       catchError((error) => {
         console.log('Error fetching ', error);
 
