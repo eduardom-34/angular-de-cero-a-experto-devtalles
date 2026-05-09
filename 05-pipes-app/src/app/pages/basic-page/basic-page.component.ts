0276932a1c3c29221b60486cb06b73ae1943bb49
CesarLinux <eduardomelchor34@gmail.com>
2026-05-08 21:55:51 -0600

    UpperCase, LowerCase y TitleCase Pipes

--- 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPageComponent {}
+export default class BasicPageComponent {
+
+  nameLower = signal('cesar');
+  nameUpper = signal('CESAR');
+  fullName = signal('ceSar CasCO');
+
+}
