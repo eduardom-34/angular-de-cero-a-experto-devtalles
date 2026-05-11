92a482301621755934d623eb3e0f294db99fd05b
CesarLinux <eduardomelchor34@gmail.com>
2026-05-10 21:56:04 -0600

    Date Pipe

--- 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
@@ -11,5 +11,8 @@ export default class BasicPageComponent {
   nameLower = signal('cesar');
   nameUpper = signal('CESAR');
   fullName = signal('ceSar CasCO');
+
+
+  customDate = signal(new Date());
 
 }
